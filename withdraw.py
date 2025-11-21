9d4cb644f51dbd090cbe911bc2f87264216480e4
harshitamachakanur-blip <harshitamachakanur@gmail.com>
2025-11-21 10:13:58 +0530

    Update withdraw.py

--- withdraw.py
+++ withdraw.py
@@ -1,16 +1,18 @@
 import sys
-if(sys.argv)==3:
-    script_name=sys.argv[0]
-    amount=float(sys.argv[1])
-    withdraw=float(sys.argv[2])
-    print("User given data")
+
+if len(sys.argv) == 3:
+    script_name = sys.argv[0]
+    amount = float(sys.argv[1])
+    withdraw = float(sys.argv[2])
+    print("User provided salary:")
 else:
-    script_name=sys.argv[0]
-    amount=10000
-    withdraw=2000
-    print("Default values")
+    script_name = sys.argv[0]
+    amount=20000
+    withdraw=10000
+    print("No input given - using default salary:")
 balance=amount-withdraw
 print("Script_name:",script_name)
 print("Amount:",amount)
 print("Withdraw:",withdraw)
 print("Balance:",balance)
+
